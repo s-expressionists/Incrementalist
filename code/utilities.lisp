5bc9e30744ab1ef9aa3e114b73003007e1b0f4b8
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2024-02-21 17:24:03 +0100

    Remove unused utility functions [SET-]CURSOR-POSITION

--- code/utilities.lisp
+++ code/utilities.lisp
@@ -8,22 +8,6 @@
                       #\( #\) #\< #\> #\[ #\] #\{ #\}
                       #\" #\' #\` #\/ #\- #\+ #\* #\% #\= #\#)))
 
-;;; Return the line number and the column number of CURSOR as two
-;;; values.
-(defun cursor-positions (cursor)
-  (values (cluffer:line-number cursor)
-          (cluffer:cursor-position cursor)))
-
-;;; Set the line number and the column number of CURSOR.
-(defun set-cursor-positions (cursor line-number column-number)
-  (let ((buffer (cluffer:buffer cursor)))
-    (when (cluffer:cursor-attached-p cursor)
-      (cluffer:detach-cursor cursor))
-    (cluffer:attach-cursor
-      cursor
-      (cluffer:find-line buffer line-number)
-      column-number)))
-
 ;;; Positions
 
 (defmacro %position< (line-number1 item-number1 line-number2 item-number2)
